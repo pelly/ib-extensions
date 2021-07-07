b8fb1e99f606edfa4756ba9026fe565a72bc01d7
Hartmut Bischoff <topofocus@gmaiol.com>
2021-07-07 17:05:21 +0000

    Adding Future#roll helper  method

---
+++ lib/ib/models/future.rb
@@ -0,0 +1,19 @@
+module IB
+  class Future
+    # helper method to roll an existing future
+    #
+    # Argument is the expiry of the target-future.
+    #
+
+    def roll **args
+      error "specify expiry to roll a future" if args.empty?
+      args[:expiry]= IB::Spread.transform_distance( expiry, args.delete(:to  )) if args[:to].present?
+      
+      new_future =  merge( **args ).verify.first
+      error "Cannot roll future; target is no IB::Contract" unless new_future.is_a? IB::Future
+      target = IB::Spread.new exchange: exchange, symbol: symbol, currency: currency
+      target.add_leg self, action:  :buy
+      target.add_leg new_future, action: :sell
+    end
+  end
+end
